708fdb1869cd557cd4b6223b4225d2bcd3755d5f
LHD <sunupdong@gmail.com>
2022-05-29 16:28:33 +0800

    处理跨域

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
 const data = require("./api/weiboHot");
 
+app.use(cors())
 app.use(express.json({ extended: false }));
-
 app.use("/api/hot", data);
 
 const PORT = process.env.PORT || 8080;
